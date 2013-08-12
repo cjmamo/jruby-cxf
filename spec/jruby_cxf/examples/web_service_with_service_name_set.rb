e12a87e77f929d0b1588f4904a79dd2d680d1771
Claude Mamo <claude.mamo@ricston.com>
2013-08-12 16:02:26 +0200

    Fixed indents

--- spec/jruby_cxf/examples/web_service_with_service_name_set.rb
+++ spec/jruby_cxf/examples/web_service_with_service_name_set.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + '/test_web_service'
 
 class WebServiceWithServiceNameSet < TestWebService
-	service_name :FooService
+    service_name :FooService
 end
--- spec/jruby_cxf/web_sevice_servlet_spec.rb
+++ spec/jruby_cxf/web_sevice_servlet_spec.rb
@@ -7,18 +7,18 @@ describe CXF::WebServiceServlet do
     subject(:wsdl) { @web_service.wsdl }
 
     before(:all) do
-        @web_service = WebServiceWithExposedMethodsHavingResponseWrapperNameSet.new
-        @web_service.publish
-      end
+      @web_service = WebServiceWithExposedMethodsHavingResponseWrapperNameSet.new
+      @web_service.publish
+    end
 
-      after(:all) do
-        @web_service.teardown
-      end
+    after(:all) do
+      @web_service.teardown
+    end
 
-      it "gives a WSDL with response wrapper elements named according to given response wrapper names" do
-        wsdl['definitions']['types']['schema']['complexType'][1]['name'].should eq 'GiveAgeResponseWrapper'
-        wsdl['definitions']['types']['schema']['complexType'][3]['name'].should eq 'SayHelloResponseWrapper'
-      end
+    it "gives a WSDL with response wrapper elements named according to given response wrapper names" do
+      wsdl['definitions']['types']['schema']['complexType'][1]['name'].should eq 'GiveAgeResponseWrapper'
+      wsdl['definitions']['types']['schema']['complexType'][3]['name'].should eq 'SayHelloResponseWrapper'
+    end
 
   end
 
@@ -26,18 +26,18 @@ describe CXF::WebServiceServlet do
     subject(:wsdl) { @web_service.wsdl }
 
     before(:all) do
-        @web_service = WebServiceWithExposedMethodsHavingOutParameterNameSet.new
-        @web_service.publish
-      end
+      @web_service = WebServiceWithExposedMethodsHavingOutParameterNameSet.new
+      @web_service.publish
+    end
 
-      after(:all) do
-        @web_service.teardown
-      end
+    after(:all) do
+      @web_service.teardown
+    end
 
-      it "gives a WSDL with return value elements named according to given out parameter names" do
-        wsdl['definitions']['types']['schema']['complexType'][1]['sequence']['element']['name'].should eq 'OutGiveAge'
-        wsdl['definitions']['types']['schema']['complexType'][3]['sequence']['element']['name'].should eq 'OutSayHello'
-      end
+    it "gives a WSDL with return value elements named according to given out parameter names" do
+      wsdl['definitions']['types']['schema']['complexType'][1]['sequence']['element']['name'].should eq 'OutGiveAge'
+      wsdl['definitions']['types']['schema']['complexType'][3]['sequence']['element']['name'].should eq 'OutSayHello'
+    end
 
   end
 
@@ -65,72 +65,72 @@ describe CXF::WebServiceServlet do
 
     before(:all) do
       @web_service = WebServiceWithExposedMethods.new
-    	@web_service.publish
-  	end
+      @web_service.publish
+    end
 
-  	after(:all) do
-    	@web_service.teardown
-  	end
+    after(:all) do
+      @web_service.teardown
+    end
 
     it "gives a WSDL with declared methods exposed" do
-  			wsdl['definitions']['types']['schema']['complexType'][0]['sequence']['element']['type'].should eq 'xsd:int'
-  			wsdl['definitions']['types']['schema']['complexType'][1]['sequence']['element']['type'].should eq 'xsd:int'
-  			wsdl['definitions']['types']['schema']['complexType'][2]['sequence']['element']['type'].should eq 'xsd:string'
-  			wsdl['definitions']['types']['schema']['complexType'][3]['sequence']['element']['type'].should eq 'xsd:string'
-        wsdl['definitions']['binding']['operation'][0]['name'].should eq 'give_age'
-        wsdl['definitions']['binding']['operation'][1]['name'].should eq 'say_hello'
+      wsdl['definitions']['types']['schema']['complexType'][0]['sequence']['element']['type'].should eq 'xsd:int'
+      wsdl['definitions']['types']['schema']['complexType'][1]['sequence']['element']['type'].should eq 'xsd:int'
+      wsdl['definitions']['types']['schema']['complexType'][2]['sequence']['element']['type'].should eq 'xsd:string'
+      wsdl['definitions']['types']['schema']['complexType'][3]['sequence']['element']['type'].should eq 'xsd:string'
+      wsdl['definitions']['binding']['operation'][0]['name'].should eq 'give_age'
+      wsdl['definitions']['binding']['operation'][1]['name'].should eq 'say_hello'
     end
   end
 
   context WebServiceWithServiceNameSet do
-		subject(:wsdl) { @web_service.wsdl }
+    subject(:wsdl) { @web_service.wsdl }
 
- 		before(:all) do
-    		@web_service = WebServiceWithServiceNameSet.new
-    		@web_service.publish
-  		end
+    before(:all) do
+      @web_service = WebServiceWithServiceNameSet.new
+      @web_service.publish
+    end
 
-  		after(:all) do
-    		@web_service.teardown
-  		end
+    after(:all) do
+      @web_service.teardown
+    end
 
-    	it "gives a WSDL with the configured service name" do
-    		wsdl['definitions']['service']['name'].should eq 'FooService'
-    	end
+    it "gives a WSDL with the configured service name" do
+      wsdl['definitions']['service']['name'].should eq 'FooService'
+    end
   end
 
   context WebServiceWithEndpointNameSet do
-		subject(:wsdl) { @web_service.wsdl }
+    subject(:wsdl) { @web_service.wsdl }
 
- 		before(:all) do
-    		@web_service = WebServiceWithEndpointNameSet.new
-    		@web_service.publish
-  		end
+    before(:all) do
+      @web_service = WebServiceWithEndpointNameSet.new
+      @web_service.publish
+    end
 
-  		after(:all) do
-    		@web_service.teardown
-  		end
+    after(:all) do
+      @web_service.teardown
+    end
 
-    	it "gives a WSDL with the port name matching the configured endpoint name" do
-    		wsdl['definitions']['service']['port']['name'].should eq 'FooPort'
-    	end
-  end	
+    it "gives a WSDL with the port name matching the configured endpoint name" do
+      wsdl['definitions']['service']['port']['name'].should eq 'FooPort'
+    end
+  end
 
   context WebServiceWithServiceNamespaceSet do
-		subject(:wsdl) { @web_service.wsdl }
+    subject(:wsdl) { @web_service.wsdl }
 
- 		before(:all) do
-    		@web_service = WebServiceWithServiceNamespaceSet.new
-    		@web_service.publish
-  		end
+    before(:all) do
+      @web_service = WebServiceWithServiceNamespaceSet.new
+      @web_service.publish
+    end
 
-  		after(:all) do
-    		@web_service.teardown
-  		end
+    after(:all) do
+      @web_service.teardown
+    end
 
-    	it "gives a WSDL with the service target namespace matching the configured service namespace" do
-    		wsdl['definitions']['targetNamespace'].should eq 'http://jruby-cxf.org'
-    	end
-  end			
+    it "gives a WSDL with the service target namespace matching the configured service namespace" do
+      wsdl['definitions']['targetNamespace'].should eq 'http://jruby-cxf.org'
+    end
+  end
 
 end
